4ce58828a2bc2e28758b94ec5a1005b68d19d4fd
aishnaagupta <aishnaa0011@gmail.com>
2026-07-23 02:01:58 +0530

    Reverse of a number- method 1

---
+++ Reverse1.java
@@ -0,0 +1,19 @@
+public class Reverse1 {
+    static int sum = 0;
+
+    static void rev1(int n) {
+        if (n == 0) {
+            return;
+        }
+        int rem = n % 10;
+        sum = sum * 10 + rem;
+        rev1(n / 10);
+    }
+
+    public static void main(String[] args) {
+        int n = 5764;
+        rev1(n);
+        System.out.println(sum);
+    }
+
+}
