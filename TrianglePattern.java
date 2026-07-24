8b6b9f250ccf6146a81dce874ec33549999b4c74
aishnaagupta <aishnaa0011@gmail.com>
2026-07-24 23:06:00 +0530

    Pattern ques 1

---
+++ TrianglePattern.java
@@ -0,0 +1,18 @@
+public class TrianglePattern {
+    public static void main(String[] args) {
+        print(4, 0);
+    }
+
+    static void print(int r, int c) {
+        if (r == 0) {
+            return;
+        }
+        if (c < r) {
+            System.out.print("* ");
+            print(r, c + 1);
+        } else {
+            System.out.println();
+            print(r - 1, 0);
+        }
+    }
+}
