66938c80de4bc884af3826b6c8059ea9cfd4f784
aishnaagupta <aishnaa0011@gmail.com>
2026-07-24 23:10:03 +0530

    Triangle Patterns

--- TrianglePattern.java
+++ TrianglePattern.java
@@ -1,6 +1,7 @@
 public class TrianglePattern {
     public static void main(String[] args) {
         print(4, 0);
+        normalTriangle(4,0);
     }
 
     static void print(int r, int c) {
@@ -13,6 +14,19 @@ public class TrianglePattern {
         } else {
             System.out.println();
             print(r - 1, 0);
+        }
+    }
+
+    static void normalTriangle(int r, int c) {
+        if (r == 0) {
+            return;
+        }
+        if (c < r) {
+            normalTriangle(r, c + 1);
+            System.out.print("* ");
+        } else {
+            normalTriangle(r - 1, 0);
+            System.out.println();
         }
     }
 }
