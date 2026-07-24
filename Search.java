3c29b49b0a4bc5a6001a4defb366b8ec3be4b9d1
aishnaagupta <aishnaa0011@gmail.com>
2026-07-24 14:58:26 +0530

    Added all types of returning index

--- Search.java
+++ Search.java
@@ -1,13 +1,19 @@
+import java.util.ArrayList;
+
 public class Search {
     public static void main(String[] args) {
-        int[] arr = { 1, 5, 2, 20 };
-        int target = 5;
+        int[] arr = { 1, 5, 2, 2, 20 };
+        int target = 2;
 
-        if (search(arr, target, 0)) {
-            System.out.println("Target found at: " + find(arr, target, 0));
-        } else {
-            System.out.println("Target not found");
-        }
+        System.out.println(search(arr, target, 0));
+        System.out.println(findIndex(arr, target, 0));
+        System.out.println(findIndexlast(arr, target, arr.length - 1));
+
+        findIndexAll1(arr, target, 0);
+        System.out.println(list);
+
+        System.out.println(findIndexAll2(arr, target, 0, new ArrayList<>()));
+        System.out.println(findIndexAll3(arr, target, 0));
     }
 
     static boolean search(int[] arr, int target, int i) {
@@ -17,11 +23,61 @@ public class Search {
         return arr[i] == target || search(arr, target, i + 1);
     }
 
-    static int find(int[] arr, int target, int i) {
+    static int findIndex(int[] arr, int target, int i) {
+        if (i == arr.length) {
+            return -1;
+        }
         if (arr[i] == target) {
             return i;
         } else {
-            return find(arr, target, i + 1);
+            return findIndex(arr, target, i + 1);
         }
+    }
+
+    static int findIndexlast(int[] arr, int target, int i) {
+        if (i == -1) {
+            return -1;
+        }
+        if (arr[i] == target) {
+            return i;
+        } else {
+            return findIndexlast(arr, target, i - 1);
+        }
+    }
+
+    static ArrayList<Integer> list = new ArrayList<>();
+
+    static void findIndexAll1(int[] arr, int target, int i) {
+        if (i == arr.length) {
+            return;
+        }
+        if (arr[i] == target) {
+            list.add(i);
+        }
+        findIndexAll1(arr, target, i + 1);
+    }
+
+    static ArrayList findIndexAll2(int[] arr, int target, int i, ArrayList<Integer> list) {
+        if (i == arr.length) {
+            return list;
+        }
+        if (arr[i] == target) {
+            list.add(i);
+        }
+        return findIndexAll2(arr, target, i + 1, list);
+    }
+
+    // Initializing ArrayList inside function
+    static ArrayList findIndexAll3(int[] arr, int target, int i) {
+        ArrayList<Integer> list = new ArrayList<>(); //this creates a new list on every call
+        if (i == arr.length) {
+            return list;
+        }
+        if (arr[i] == target) {
+            list.add(i);
+        }
+        ArrayList<Integer> ansFromBefore = findIndexAll3(arr, target, i + 1);
+        list.addAll(ansFromBefore);
+        return list;
     }
 }
